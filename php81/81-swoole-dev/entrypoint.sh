6e955ebb830ee566fce9b7ccf07b6583564ff166
Márcio Dias <marciojr91@gmail.com>
2022-06-06 13:02:45 -0300

    feat: add swoole

---
+++ php81/81-swoole-dev/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "xdebug.client_host=`ip route|awk '/default/ { print $3 }'`" | tee -a /etc/php/8.1/fpm/conf.d/25-xdebug.ini > /dev/null
+echo "xdebug.client_host=`ip route|awk '/default/ { print $3 }'`" | tee -a /etc/php/8.1/cli/conf.d/25-xdebug.ini > /dev/null
+
+/usr/sbin/php-fpm8.1 -O
